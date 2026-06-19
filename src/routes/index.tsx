db8eef1db0cf10525b34cafe569323ee8f6610d2
Talha Naaeem <talha.naeem97@gmail.com>
2026-06-19 19:18:30 +0500

    fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,14 @@
+import { supabase } from "@/integrations/supabase/client";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  beforeLoad: () => {
-    throw redirect({ to: "/home" });
+  beforeLoad: async () => {
+    const { data } = await supabase.auth.getSession();
+
+    if (data.session) {
+      throw redirect({ to: "/home" });
+    }
+
+    throw redirect({ to: "/auth" });
   },
 });
