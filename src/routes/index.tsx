a95144abfb9889dbb3e363c66027c79417382a61
Talha Naaeem <talha.naeem97@gmail.com>
2026-06-19 15:14:34 +0500

    fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  ssr: false,
   beforeLoad: () => {
     throw redirect({ to: "/home" });
   },
